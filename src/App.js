db7d51c5cb5884cc3429c125752adc732be0e4d6
abby <abby@macbook.local>
2020-09-07 14:58:18 -0400

    I'm fun

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const tasks = ["Laundry", "Mow the Grass", "Test"];
+  var do_when = "";
+
+  console.log(tasks);
+
+  function randomColor() {
+    return Math.round(Math.random()*250);
+  }
+
   return (
+    <div>
+      <ul>
+        {tasks.map((task_in_list, array_position) => 
+          (
+            
+            <li tag = {array_position} 
+            style={{
+              color:"rgb("+randomColor()+","+randomColor()+","+randomColor()+")"
+            }}
+            >
+              {array_position+1}: {task_in_list}
+            </li>
+          )
+        )}
+      </ul>
+    </div>
+  /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,8 +45,33 @@ function App() {
         >
           Learn React
         </a>
+      
+
+        <h1>Hello World</h1>
+        <ul>
+          <li>Do laundry</li>
+          <li>Do laundry</li>
+          <li>Do laundry</li>
+          <li>Do laundry</li>
+        </ul>
+
+        <div class="center_text">
+        <p>
+        &#10024; &#128065;&#128068;&#128065; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+          Put some text here.  Put some text here.  Put some text here.  Put some text here.
+        </p>
+        </div>
+
       </header>
+
     </div>
+    */
   );
 }
 
